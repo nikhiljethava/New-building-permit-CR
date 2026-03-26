79c373aa7853ba5589c36f68331cc392e154cefc
srinandan <srinandans@google.com>
2026-03-26 05:18:09 +0000

    chore: fif format

--- api/internal/telemetry/telemetry.go
+++ api/internal/telemetry/telemetry.go
@@ -22,9 +22,9 @@ import (
 	"os"
 
 	"go.opentelemetry.io/contrib/detectors/gcp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
